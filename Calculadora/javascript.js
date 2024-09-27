228017426e204cbff4f086633372dfd72385eaba
DESKTOP-Q9Q686T\dmelnez <csi23-dmelnez@colegioaltair.net>
2024-09-27 08:14:17 +0200

    Calculadora Mejorada

--- Calculadora/javascript.js
+++ Calculadora/javascript.js
@@ -52,9 +52,43 @@ function pruebas(){
           let celda2 = nuevaFila.insertCell(1);
           let celda3 = nuevaFila.insertCell(2);
 
+            /*
           // Agregar contenido a las nuevas celdas
           celda1.innerHTML = numeroAumento++;
           celda2.innerHTML = resultadosGenerados[resultadosGenerados.length -1];
-          celda3.innerHTML = operacionesGeneradas[operacionesGeneradas.length -1];
+          celda3.innerHTML = operacionesGeneradas[operacionesGeneradas.length -1];*/
+
+          for(var i = 0; i <= resultadosGenerados.length; i++){
+            celda1.innerHTML = numeroAumento++;
+            celda2.innerHTML = resultadosGenerados[resultadosGenerados.length -1];
+            celda3.innerHTML = operacionesGeneradas[operacionesGeneradas.length -1];
+
+          }
+
+}
+
+function mostrarTodosResultados(){
+
+              // Obtener la tabla
+              let tabla = document.getElementById("tablaHistorial").getElementsByTagName('tbody')[0];
+
+              // Crear una nueva fila
+              let nuevaFila = tabla.insertRow();
+    
+              // Insertar nuevas celdas
+              let celda1 = nuevaFila.insertCell(0);
+              let celda2 = nuevaFila.insertCell(1);
+              let celda3 = nuevaFila.insertCell(2);
+    
+              // Agregar contenido a las nuevas celdas
+
+              for(var i = 0; i< resultadosGenerados.length -1; i++){
+                celda1.innerHTML = numeroAumento++;
+                celda2.innerHTML = resultadosGenerados[resultadosGenerados.length -1];
+                celda3.innerHTML = operacionesGeneradas[operacionesGeneradas.length -1];
+
+              }
+
+            
 
 }
